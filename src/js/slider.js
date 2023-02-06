12bd53256515449bb5037afad1c398ac012fe217
STIg89 <andreystig89@gmail.com>
2023-02-06 22:33:51 +0200

    slider autoplay

--- src/js/slider.js
+++ src/js/slider.js
@@ -11,6 +11,11 @@ const swiper = new Swiper('.mySwiper', {
   loop: true,
   direction: 'horizontal',
   spaceBetween: 60,
+  speed: 10000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
